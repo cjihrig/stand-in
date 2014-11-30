b9619b51b762a08010471a2bde08a5f5abd10c8c
Adam Bretz <arbretz@gmail.com>
2014-11-30 13:59:46 -0500

    Updated example.

--- examples/one.js
+++ examples/one.js
@@ -1,54 +1,44 @@
 // This example uses Lab because that's the testing framework for the project.
 // However, it could just as easily be any other framework
 
-var Lab = require('lab');
-var lab = exports.lab = Lab.script();
-var StandIn = require('../');
 var Fs = require('fs');
+var Code = require('code');
+var StandIn = require('../');
+var expect = Code.expect;
 
-// Declare test aliases
-var describe = lab.describe;
-var it = lab.it;
-var expect = Lab.expect;
 
-describe('test one', function () {
+// Here we want to make sure the error part of the code is executed, so we intercept console.log
+// and check the value
+StandIn.replace(console, 'error', function (stand, value) {
 
-  it('logs an error opening a file', function (done) {
+  stand.restore();
+  expect(value.code).to.equal('ENOENT');
+  process.exit(0);
+});
 
-    // Here we want to make sure the error part of the code is executed, so we intercept console.log
-    // and check the value
-    var error = StandIn.replace(console, 'error', function (value) {
+// We have replaced readFile with a function that always throw an error
+StandIn.replace(Fs, 'readFile', function (stand, path, callback) {
 
-      error.restore();
-      expect(value.code).to.equal('ENOENT');
-      done();
-    });
-
-    // We have replaced readFile with a function that always throw an error
-    var readFile = StandIn.replace(Fs, 'readFile', function (path, callback) {
-
-      readFile.restore();
-      expect(path).to.equal('README.md');
-      callback({
-        code: 'ENOENT'
-      });
-    });
-
-    var logger = {
-      getLog: function() {
-
-        Fs.readFile('README.md', function (error, data) {
-
-          if (error) {
-            console.error(error);
-          }
-          else {
-            console.log(data + '');
-          }
-        });
-      }
-    };
-
-    logger.getLog();
+  stand.restore();
+  expect(path).to.equal('README.md');
+  callback({
+    code: 'ENOENT'
   });
 });
+
+var logger = {
+  getLog: function() {
+
+    Fs.readFile('README.md', function (error, data) {
+
+      if (error) {
+        console.error(error);
+      }
+      else {
+        console.log(data + '');
+      }
+    });
+  }
+};
+
+logger.getLog();
